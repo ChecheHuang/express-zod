5ca8d1b2059a93c38c077203776eb654a57df7b8
Carl Huang <carl.huang@pstarc.com>
2024-07-16 17:30:25 +0800

    master(master): 123
    需求
    
    123
    
    調整
    
    123

--- src/utils/procedure.ts
+++ src/utils/procedure.ts
@@ -1,8 +1,30 @@
 import { authMiddleware } from '@/middleware/authMiddleware'
 import { defaultEndpointsFactory } from 'express-zod-api'
+import { z } from 'zod'
 
 export const procedure = defaultEndpointsFactory.build.bind(defaultEndpointsFactory)
 
 export const privateProcedure = defaultEndpointsFactory
   .addMiddleware(authMiddleware)
   .build.bind(defaultEndpointsFactory.addMiddleware(authMiddleware))
+
+const aaa = defaultEndpointsFactory.build({
+  tag: '123',
+  method: 'get',
+  input: z.object({name:z.string()}),
+  output: z.object({}),
+  handler: async ({ input, options, logger }) => {
+    return {}
+  },
+})
+
+
+
+// const aaa2 = fn
+// .method('get')
+// .tag('123')
+// .input(z.object({}))
+// .output(z.object({}))
+// .handler(async ({ input, options, logger }) => {
+//   return {}
+// }
