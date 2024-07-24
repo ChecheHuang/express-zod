4c3aa48e1ee62d13d8bd6007fc5000d5a0bf776d
Carl <qwerfcxzas1@gmail.com>
2024-07-24 23:49:13 +0800

    moidfy

---
+++ client/src/lib/socket-implementation.ts
@@ -0,0 +1,20 @@
+/* eslint-disable @typescript-eslint/no-namespace */
+  import type { Socket as SocketBase } from "socket.io-client";
+
+export namespace Root {
+    /** @desc The actual path of the Root namespace */
+    export const path = "/";
+    export interface Emission {
+        onChat: (p1: string, p2: {
+            from: string;
+        }) => void;
+    }
+    export interface Actions {
+        ping: ((cb1: (p1: "pong", ...rest: unknown[]) => void) => void) | ((rest1: unknown, cb2: (p1: "pong", ...rest: unknown[]) => void) => void) | ((rest1: unknown, rest2: unknown, cb3: (p1: "pong", ...rest: unknown[]) => void) => void);
+        subscribe: (...doesNotMatter: unknown[]) => void;
+        unsubscribe: (...doesNotMatter: unknown[]) => void;
+        chat: (message: string) => void;
+    }
+    /** @example const socket: Root.Socket = io(Root.path) */
+    export type Socket = SocketBase<Emission, Actions>;
+}
