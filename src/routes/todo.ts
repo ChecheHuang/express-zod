32c77d794aabd46faaf3eb381f9b497352b48ea1
Carl Huang <carl.huang@pstarc.com>
2024-07-26 14:36:49 +0800

    master(master): 123
    需求
    
    123
    
    調整
    
    123

--- src/routes/todo.ts
+++ src/routes/todo.ts
@@ -1,4 +1,6 @@
+import prismadb from '@/utils/prismadb'
 import { privateProcedure, procedure } from '@/utils/procedure'
+import { todoSchema } from '@/zodSchemas'
 import { defaultEndpointsFactory, DependsOnMethod, Routing } from 'express-zod-api'
 import { z } from 'zod'
 
@@ -10,18 +12,34 @@ const todo: Routing = {
       tag,
       method: 'get',
       input: z.object({}),
-      output: z.object({}),
+      output: z
+        .object({
+          todos: z.array(todoSchema),
+        })
+        .example({
+          todos: [{ id: 1, title: 'string', content: 'string', status: true }],
+        }),
       handler: async ({ input, options, logger }) => {
-        return {}
+        const todos = await prismadb.todo.findMany()
+        return { todos }
       },
     }),
     post: defaultEndpointsFactory.build({
       tag,
       method: 'post',
-      input: z.object({}),
-      output: z.object({}),
+      input: todoSchema.omit({ id: true }).example({
+        title: 'string',
+        content: 'string',
+        status: true,
+      }),
+      output: todoSchema.example({
+        id: 1,
+        title: 'string',
+        content: 'string',
+        status: true,
+      }),
       handler: async ({ input, options, logger }) => {
-        return {}
+        return await prismadb.todo.create({ data: input })
       },
     }),
   }),
@@ -32,30 +50,59 @@ const todo: Routing = {
       input: z.object({
         id: z.string().transform((id) => parseInt(id, 10)),
       }),
-      output: z.object({}),
+      output: todoSchema.example({
+        id: 1,
+        title: 'string',
+        content: 'string',
+        status: true,
+      }),
       handler: async ({ input, options, logger }) => {
         console.log(input.id)
-        return {}
+        const todo = await prismadb.todo.findUnique({
+          where: { id: input.id },
+        })
+        if (!todo) throw new Error('找不到資料')
+        return todo
       },
     }),
     put: defaultEndpointsFactory.build({
       tag,
       method: 'put',
-      input: z.object({
-        id: z.string().transform((id) => parseInt(id, 10)),
+      input: todoSchema.omit({ id: true }).extend({ id: z.string().transform((id) => parseInt(id, 10)) }),
+      output: todoSchema.example({
+        id: 1,
+        title: 'string',
+        content: 'string',
+        status: true,
       }),
-      output: z.object({}),
-      handler: async ({ input, options, logger }) => {
-        return {}
+      handler: async ({ input: { id, ...data }, options, logger }) => {
+        const exist = !!(await prismadb.todo.findUnique({
+          where: { id },
+        }))
+        if (!exist) throw new Error('找不到資料')
+        const todo = await prismadb.todo.update({
+          where: { id: id },
+          data,
+        })
+        return todo
       },
     }),
     delete: privateProcedure({
       tag,
       method: 'delete',
-      input: z.object({}),
-      output: z.object({}),
-      handler: async ({ input, options, logger }) => {
-        return {}
+      input: z.object({
+        id: z.string().transform((id) => parseInt(id, 10)),
+      }),
+      output: todoSchema.example({
+        id: 1,
+        title: 'string',
+        content: 'string',
+        status: true,
+      }),
+      handler: async ({ input: { id }, options, logger }) => {
+        return await prismadb.todo.delete({
+          where: { id },
+        })
       },
     }),
   }),
